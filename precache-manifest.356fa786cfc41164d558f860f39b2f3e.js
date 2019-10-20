7a1846699f874e2254de04c278311c696dcd261b
Bob <bobhsiao.cs07@nctu.edu.tw>
2019-10-20 10:24:40 +0800

    Updates

---
+++ precache-manifest.356fa786cfc41164d558f860f39b2f3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9960b5aa23709dfcff45140f42c0ff87",
+    "url": "/NasaHackathonProject/index.html"
+  },
+  {
+    "revision": "fee3367339fe16023dfa",
+    "url": "/NasaHackathonProject/static/css/main.b59ec892.chunk.css"
+  },
+  {
+    "revision": "1076583c65e68edcc11d",
+    "url": "/NasaHackathonProject/static/js/2.b568ede8.chunk.js"
+  },
+  {
+    "revision": "fee3367339fe16023dfa",
+    "url": "/NasaHackathonProject/static/js/main.684f3b6b.chunk.js"
+  },
+  {
+    "revision": "59c9937555404f8c6829",
+    "url": "/NasaHackathonProject/static/js/runtime-main.e6c87f05.js"
+  },
+  {
+    "revision": "bd589cebcd28cd6a0a6e04314fd2c44b",
+    "url": "/NasaHackathonProject/static/media/china.bd589ceb.jpg"
+  },
+  {
+    "revision": "c530a2a4874a662da6a78d22d8cd4458",
+    "url": "/NasaHackathonProject/static/media/india.c530a2a4.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/NasaHackathonProject/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e74a615381aca02e655919e2cef2fc71",
+    "url": "/NasaHackathonProject/static/media/taiwan.e74a6153.jpg"
+  },
+  {
+    "revision": "b7ef8203b74db090a87fbccd33d36ac2",
+    "url": "/NasaHackathonProject/static/media/us.b7ef8203.jpg"
+  }
+]);
