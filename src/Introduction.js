47e86e89a7bb8ca08ed07f5c8362caec2999f4fa
Bob <bobhsiao.cs07@nctu.edu.tw>
2019-10-20 12:02:35 +0800

    second commit

--- src/Introduction.js
+++ src/Introduction.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import './bootstrap.css';
 import './Introduction.css';
 import {Link} from 'react-router-dom'
-import pic from './pic/images.jpg'
+import Pic from './pic/air_pollution.png'
 /*
 <link
   rel="stylesheet"
@@ -18,14 +18,14 @@ class Introduction extends React.Component{
             <div className="IntroductionBackground">
                 <div className="IntroductionContent">
                     <div className="IntroductionText">
-                      <div className="Text">
-                        <h1 className="text1">Check out local air pollution!</h1>
-                        <h3 className="text2">Search for latest news about air pollution</h3>
-                        <h3 className="text3">or click the button below to search your country on map!</h3>
-                      </div>
+                        <div className="Text">
+                          <h1 className="text1">Check out local air pollution!</h1>
+                          <h3 className="text2">Learn about air pollution and some useful tips</h3>
+                          <h3 className="text3">or click the button below to search your country on map!</h3>
+                        </div>
                     </div>
                     <div className="Btn">
-                      <a href='https://l4ll1p0p.github.io/2019NASA_Hackathon/?fbclid=IwAR3w_ZdO_ypsknu9LXoL4dKgMxJgxP2tQ3b5cHhhnkNMuEtaH618m3_QIDE'>
+                      <a href='https://l4ll1p0p.github.io/2019NASA_Hackathon/'>
                       <button className="button" onClick={this.ToMap}>
                         VIEW NOW
                       </button>
